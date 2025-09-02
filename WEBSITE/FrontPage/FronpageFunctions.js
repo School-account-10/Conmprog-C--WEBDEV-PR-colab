e941e484ba592c7904fd483b4615d715ae29718b
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-09-02 16:23:02 +0800

    Add age group definitions in weight_lim function
    Introduced variables for different age groups within the weight_lim function to categorize users by age ranges. This sets up the groundwork for age-based logic related to weight limits.

--- WEBSITE/FrontPage/FronpageFunctions.js
+++ WEBSITE/FrontPage/FronpageFunctions.js
@@ -13,6 +13,33 @@ updateWeight.call(document.getElementById('Weight_slider'));
 
 function weight_lim(){
     const weight_age_limit = document.getElementById('Age_picker').value // so i can get data from the age
+    var young_age_group = {
+        from: 1,
+        to: 17
+    };
+
+    var young_adult_group = {
+        from: 18,
+        to: 29
+    };
+
+    var middle_adult = {
+        from: 30,
+        to: 49
+    };
+
+    var adult_na_talaga = { // early stage ng sakt sa likod at atritis
+        from: 50, 
+        to: 59
+    };
+
+    var elderly = {
+        from: 56,
+        to: 100
+    };
+
+
+ }
+ 
     
     
-}
