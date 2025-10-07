5590b7e90e726a09c0c10e174fece7b8ae365f02
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-10-07 21:40:16 +0800

    Update build outputs and add iLS.zip archive
    Added iLS.zip to the repository and updated various build artifacts, including DLLs, EXEs, PDBs, and cache files. Minor changes to Visual Studio project metadata and document layout files are also included.

--- C#/iLS/iLS/obj/Debug/net8.0-windows/iLS.AssemblyInfo.cs
+++ C#/iLS/iLS/obj/Debug/net8.0-windows/iLS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iLS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b6c97db40f858dd930e8801a6df1f34fe7aec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93356ea8ef122575ad646b4ee4d6c5cd15209cc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("iLS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iLS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
