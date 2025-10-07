429e148f39c6f26f8cd657e00afab42b07860446
School-account-10 <160393316+School-account-10@users.noreply.github.com>
2025-10-07 22:32:50 +0800

    ;

--- C#/WEBSITE/FrontPage/loadNavbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-      const navbarContainer = document.getElementById("navbar-container");
-      navbarContainer.innerHTML = data;
-    })
-    .catch(error => console.error("Error loading navbar:", error));
-});
